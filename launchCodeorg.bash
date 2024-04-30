7b5e90aa1674b3b4aba1325ccbb6789cf6b02dda
Liam Harkness <rocketliam@Wisp.local>
2024-04-30 12:17:26 -0400

    Bash script and ES config

--- launchCodeorg.bash
+++ launchCodeorg.bash
@@ -2,10 +2,6 @@
 
 long_path=$1
 
-short_path=${long_path##*/}
+short_path= "$(basename $(dirname $long_path))/$(basename $long_path)"
 
-touch /Users/rocketliam/ROMs/code.org/remix/log
-
-echo $short_path > /Users/rocketliam/ROMs/code.org/remix/log 
-
-/Applications/Firefox.app/Contents/MacOS/firefox -kiosk "http://127.0.0.1:8080/$short_path"  
+firefox -kiosk "http://127.0.0.1:8080/$short_path"  
